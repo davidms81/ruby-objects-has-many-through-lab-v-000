817ef9a273c4c0d20ed26bb3fe89e4438d461da4
David Schoenholtz <dmschoenholtz@gmail.com>
2019-12-09 21:43:32 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,8 +12,8 @@ class Artist
   end
 
   def new_song(name, genre)
-    Song.new(name, self, genre)
-    @@all << self
+    song = Song.new(name, self, genre)
+    @@all << song
   end
 
   def songs
